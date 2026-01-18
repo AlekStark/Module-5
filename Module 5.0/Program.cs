8f31bda90a481214e1a3442bd052a71485b5041c
Aleksandr <alekstark@live.ru>
2026-01-18 19:36:23 +0300

    5.0

--- Module 5.0/Program.cs
+++ Module 5.0/Program.cs
@@ -1,26 +1,26 @@
 ﻿using System;
 using static System.Console;
 
-//namespace Module5
-//{
+namespace Module5
+{
     class MainClass
     {
-        private static void Main(string[] args)
+        public static void Main(string[] args)
         {
             (string Name, string[] Dishes) User;
             WriteLine("Введите имя");
             User.Name = ReadLine();
             User.Dishes = new string[5];
-            
+
             for (int i = 0; i < User.Dishes.Length; i++)
             {
-            WriteLine("Введите название блюда номер {0}", i + 1);
+                WriteLine("Введите название блюда номер {0}", i + 1);
                 User.Dishes[i] = ReadLine();
             }
-            ReadLine();
+            ReadKey();
 
 
             //Console.WriteLine("Hello, World!");
         }
     }
-//}
+}
