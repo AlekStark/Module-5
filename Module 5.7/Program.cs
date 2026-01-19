b36495d05c5c017b15b1687feacd759f79ff5af4
Aleksandr <alekstark@live.ru>
2026-01-19 21:54:23 +0300

    5.7

--- Module 5.7/Program.cs
+++ Module 5.7/Program.cs
@@ -1,17 +1,18 @@
 ﻿using System;
+using System.Diagnostics.Metrics;
 using static System.Console;
 
 class Program
 {
     private static void Main(string[] args)
     {
-        (string FirstName, string LastName, int Age) MainUser;
+        (string FirstName, string LastName, int Age, int HasPet, int CountPet) MainUser;
         MainUser = Enteruser();
-        WriteLine(MainUser.FirstName + " " + MainUser.LastName + " " + MainUser.Age);
+        WriteLine("Фамилия: " + MainUser.FirstName + "\nИмя: " + MainUser.LastName + "\nВозраст: " + MainUser.Age /*+ "\nКоличество питомцев: " + MainUser.Pet*/);
     }
-    static (string FirstName, string LastName, int Age) Enteruser()
+    static (string FirstName, string LastName, int Age, bool HasPet, int CountPet) Enteruser()
     {
-        (string FirstName, string LastName, int Age) User;
+        (string FirstName, string LastName, int Age, bool HasPet, int CountPet) User;
 
         WriteLine("Укажите имя:");
         User.FirstName = ReadLine();
@@ -29,6 +30,37 @@ class Program
         while (CheckNum(age, out intage));
 
         User.Age = intage;
+
+        WriteLine("Есть ли у вас животные? Да или Нет");
+        var pet = ReadLine();
+
+        if (pet == "Да")
+        {
+            string countPet;
+            do
+            {
+                
+                WriteLine("Укажите количество питомцев цифрами:");
+                countPet = ReadLine();
+            }
+            while (CheckNum(countPet, out intage));
+
+            if (intage == 0)
+            {
+                User.HasPet = false;
+                User.CountPet = 0;
+            }
+            else
+            {
+                User.HasPet = true;
+                User.CountPet = intage;
+            }
+        }
+        else
+        {
+            User.CountPet = 0;
+            User.HasPet = false;
+        }
         return User;
     }
 
