610d8a6ea0cc69de367cffe4026e051c77643bb8
Aleksandr <alekstark@live.ru>
2026-01-21 09:59:11 +0300

    5.7

--- Module 5.7/Program.cs
+++ Module 5.7/Program.cs
@@ -1,14 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.Metrics;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
 using static System.Console;
+using static System.Net.Mime.MediaTypeNames;
 
 class Program
 {
     private static void Main(string[] args)
     {
         //(string FirstName, string LastName, int Age, int HasPet, int CountPet) MainUser;
-        var MainUser = Enteruser();
+        var MainUser = Enteruser(); // Вызываем метод сбора данных
         string text = "Фамилия: " + MainUser.FirstName + "\nИмя: " + MainUser.LastName + "\nВозраст: " + MainUser.Age + "\nНаличие питомцев: " + MainUser.HasPet +
             "\nКоличество питомцев: " + MainUser.CountPet;
 
@@ -31,7 +32,7 @@ class Program
             WriteLine("Укажите возраст цифрами:");
             age = ReadLine();
         }
-        while (CheckNum(age, out intage, "Age"));
+        while (CheckNum(age, out intage)); //цикл для проверки, что введины цифры.
 
         User.Age = intage;
 
@@ -46,40 +47,29 @@ class Program
                 WriteLine("Укажите количество питомцев цифрами:");
                 countPet = ReadLine();
             }
-            while (CheckNum(countPet, out intage, "Pet"));
+            while (CheckNum(countPet, out intage));
 
-            if (intage == 0)
-            {
-                User.HasPet = false;
-                User.CountPet = 0;
-                User.PetName = new string[0];
-            }
-            else
-            {
-                User.HasPet = true;
-                User.CountPet = intage;
-                User.PetName = GetPetName(User.CountPet);
-            }
+            User.HasPet = true;
+            User.CountPet = intage;
+            User.PetName = GetPetName(User.CountPet);
         }
         else
         {
-            User.CountPet = 0;
             User.HasPet = false;
+            User.CountPet = 0;
             User.PetName = new string[0];
         }
+        WriteLine("Есть ли у вас животные? Да или Нет");
+        var pet = ReadLine();
+
         return User;
     }
 
-    static bool CheckNum(string number, out int corrnumber, string Operation)
+    static bool CheckNum(string number, out int corrnumber)
     {
         if (int.TryParse(number, out int intnum))
         {
-            if (intnum > 0 && Operation=="Age")
-            {
-                corrnumber = intnum;
-                return false;
-            }
-            else if (intnum >= 0 && Operation == "Pet")
+            if (intnum > 0)
             {
                 corrnumber = intnum;
                 return false;
@@ -88,14 +78,43 @@ class Program
         corrnumber = 0;
         return true;
     }
+
     static string[] GetPetName(int countPet)
     {
         string [] petName = new string[countPet];
         for (int i = 0; i < countPet; i++)
         {
-            WriteLine("Укажите кличку питомца {0}", i);
-            petName[i] = ReadLine();
+            string Name;
+            do
+            {
+                WriteLine("Укажите кличку питомца {0}. Имя должно быть уникальным", i);
+                Name = ReadLine();
+            }
+            while (CheckUniqueness(Name, petName, out bool result));
+            petName[i] = Name;
         }
         return petName; 
+    }
+
+    static bool CheckUniqueness(string Name, string[] arr, out bool result)
+    {
+        string Pattern = "^[a-zA-Zа-яёА-ЯЁ]+$";
+        if (Regex.IsMatch(Name, Pattern))
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (Name == arr[i])
+                {
+                    result = false;
+                    return result;
+                }
+            }
+            result = true;
+         }
+        else
+        {
+            result = false;
+        }
+        return result; 
     }
 }
